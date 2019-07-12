a1b94b3e2a860fc28727fb4b21b1b7a852e1258d
Gene Boggs <gene@ology.net>
2019-07-12 02:38:09 -0700

    Add the ->dump function

--- lib/MIDI/Util.pm
+++ lib/MIDI/Util.pm
@@ -7,6 +7,8 @@ our $VERSION = '0.0300';
 use strict;
 use warnings;
 
+use MIDI;
+use MIDI::Event;
 use MIDI::Track;
 use MIDI::Simple;
 use Music::Tempo;
@@ -131,6 +133,114 @@ sub set_chan_patch {
     $score->patch_change( $channel, $patch );
 
     $score->noop( 'c' . $channel );
+}
+
+=head2 dump()
+
+Return array references of the following L<MIDI>, L<MIDI::Simple>, and
+L<MIDI::Event> lists:
+
+  Volume
+  Length
+  Note
+  note2number
+  number2note
+  patch2number
+  number2patch
+  notenum2percussion
+  percussion2notenum
+  All_events
+  MIDI_events
+  Meta_events
+  Text_events
+  Nontext_meta_events
+
+=cut
+
+sub dump {
+    my ($key) = @_;
+
+    if ( lc $key eq 'volume' ) {
+        return [
+            map { "$_ => $MIDI::Simple::Volume{$_}" }
+                sort { $MIDI::Simple::Volume{$a} <=> $MIDI::Simple::Volume{$b} }
+                    keys %MIDI::Simple::Volume
+        ];
+    }
+    elsif ( lc $key eq 'length' ) {
+        return [
+            map { "$_ => $MIDI::Simple::Length{$_}" }
+                sort { $MIDI::Simple::Length{$a} <=> $MIDI::Simple::Length{$b} }
+                    keys %MIDI::Simple::Length
+        ];
+    }
+    elsif ( lc $key eq 'note' ) {
+        return [
+            map { "$_ => $MIDI::Simple::Note{$_}" }
+                sort { $MIDI::Simple::Note{$a} <=> $MIDI::Simple::Note{$b} }
+                    keys %MIDI::Simple::Note
+        ];
+    }
+    elsif ( lc $key eq 'note2number' ) {
+        return [
+            map { "$_ => $MIDI::note2number{$_}" }
+                sort { $MIDI::note2number{$a} <=> $MIDI::note2number{$b} }
+                    keys %MIDI::note2number
+        ];
+    }
+    elsif ( lc $key eq 'number2note' ) {
+        return [
+            map { "$_ => $MIDI::number2note{$_}" }
+                sort { $a <=> $b }
+                    keys %MIDI::number2note
+        ];
+    }
+    elsif ( lc $key eq 'patch2number' ) {
+        return [
+            map { "$_ => $MIDI::patch2number{$_}" }
+                sort { $MIDI::patch2number{$a} <=> $MIDI::patch2number{$b} }
+                    keys %MIDI::patch2number
+        ];
+    }
+    elsif ( lc $key eq 'number2patch' ) {
+        return [
+            map { "$_ => $MIDI::number2patch{$_}" }
+                sort { $a <=> $b }
+                    keys %MIDI::number2patch
+        ];
+    }
+    elsif ( lc $key eq 'notenum2percussion' ) {
+        return [
+            map { "$_ => $MIDI::notenum2percussion{$_}" }
+                sort { $a <=> $b }
+                    keys %MIDI::notenum2percussion
+        ];
+    }
+    elsif ( lc $key eq 'percussion2notenum' ) {
+        return [
+            map { "$_ => $MIDI::percussion2notenum{$_}" }
+                sort { $MIDI::percussion2notenum{$a} <=> $MIDI::percussion2notenum{$b} }
+                    keys %MIDI::percussion2notenum
+        ];
+    }
+    elsif ( lc $key eq 'all_events' ) {
+        return \@MIDI::Event::All_events;
+    }
+    elsif ( lc $key eq 'midi_events' ) {
+        return \@MIDI::Event::MIDI_events;
+    }
+    elsif ( lc $key eq 'meta_events' ) {
+        return \@MIDI::Event::Meta_events;
+    }
+    elsif ( lc $key eq 'text_events' ) {
+        return \@MIDI::Event::Text_events;
+    }
+    elsif ( lc $key eq 'nontext_meta_events' ) {
+        return \@MIDI::Event::Nontext_meta_events;
+    }
+    else {
+        return [];
+    }
 }
 
 1;
